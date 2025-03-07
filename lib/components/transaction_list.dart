9c9872b73270c451cd60331f8420ae92e98acd8e
Ariane <ariane@globalfy.com>
2025-03-07 13:00:48 -0300

    feat: added media query on landscape orientation and aplied height to fix small screens

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,57 +10,60 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 430,
-      child:
-          transactions.isEmpty
-              ? Column(
-                children: <Widget>[
-                  SizedBox(height: 20),
-                  Text(
+    return transactions.isEmpty
+        ? LayoutBuilder(
+          builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                SizedBox(height: constraints.maxHeight * 0.05),
+                Container(
+                  height: constraints.maxHeight * 0.2,
+                  child: Text(
                     'Nenhuma transação cadastrada',
                     style: Theme.of(context).textTheme.titleLarge,
                   ),
-                  SizedBox(height: 40),
-                  Container(
-                    height: 200,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    ),
+                ),
+                SizedBox(height: constraints.maxHeight * 0.05),
+                Container(
+                  height: constraints.maxHeight * 0.5,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                ],
-              )
-              : ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (ctx, index) {
-                  final tr = transactions[index];
-                  return Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Theme.of(context).colorScheme.primary,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6),
-                          child: FittedBox(child: Text('R\$${tr.value}')),
-                        ),
-                      ),
-                      title: Text(
-                        tr.title,
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      subtitle: Text(DateFormat('d MMM y').format(tr.date)),
-                      trailing: IconButton(
-                        onPressed: () => onRemove(tr.id),
-                        color: Theme.of(context).colorScheme.error,
-                        icon: Icon(Icons.delete),
-                      ),
-                    ),
-                  );
-                },
+                ),
+              ],
+            );
+          },
+        )
+        : ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            final tr = transactions[index];
+            return Card(
+              elevation: 5,
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  child: Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: FittedBox(child: Text('R\$${tr.value}')),
+                  ),
+                ),
+                title: Text(
+                  tr.title,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                subtitle: Text(DateFormat('d MMM y').format(tr.date)),
+                trailing: IconButton(
+                  onPressed: () => onRemove(tr.id),
+                  color: Theme.of(context).colorScheme.error,
+                  icon: Icon(Icons.delete),
+                ),
               ),
-    );
+            );
+          },
+        );
   }
 }
